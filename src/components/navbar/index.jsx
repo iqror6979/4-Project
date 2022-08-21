fd0627ac5f996886d0b1093d7db9be9b331becfd
iqror6979 <iqrorbekmirzaxatamov@gmail.com>
2022-08-21 13:18:32 +0500

    navbardi push qildim

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,188 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+import { NavLink } from "react-router-dom";
+
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+
+import { FaFacebookF } from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs";
+import { FiInstagram } from "react-icons/fi";
+import { GrDribbble } from "react-icons/gr";
+import { Typography } from "@mui/material";
+
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+import navbar from "../../assets/navbar.jpg";
+
+export default () => {
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      color: isActive ? "blue" : "black",
+      textDecoration: isActive ? "none" : "none",
+      paddingLeft: "2%",
+    };
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        // position: 'sticky',
+        // top:0,
+        zIndex: 10,
+      }}
+    >
+      <Box
+        sx={{
+          backgroundColor: "#333",
+          paddingTop: "7px",
+          paddingBottom: "7px",
+          display: "flex ",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box>
+          <h6
+            style={{
+              color: "white",
+              fontFamily: "Sen, sans-serif",
+              fontSize: "13px",
+              paddingLeft: "30%",
+              width: "100%",
+            }}
+          >
+            Free shipping for standard order over $100
+          </h6>
+        </Box>
+        <Box sx={{ pr: "3%", pt: "2px" }}>
+          <FaFacebookF style={{ padding: "0px 10px", color: "white" }} />
+          <BsTwitter style={{ padding: "0px 10px", color: "white" }} />
+          <FiInstagram style={{ padding: "0px 10px", color: "white" }} />
+          <GrDribbble style={{ padding: "0px 10px", color: "white" }} />
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "#fff",
+          paddingTop: "20px",
+          paddingBottom: "20px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <img
+            src={navbar}
+            alt=""
+            style={{ paddingLeft: "13%", paddingRight: "7%" }}
+          />
+          <NavLink to="/" style={navLinkStyles}>
+            <ListItem disablePadding>
+              <ListItemButton
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "white",
+                    borderRadius: "5px",
+                    color: "blue",
+                  },
+                }}
+              >
+                <ListItemText primary="Home" />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+          <NavLink to="/shop" style={navLinkStyles}>
+            <ListItem disablePadding>
+              <ListItemButton
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "white",
+                    borderRadius: "5px",
+                    color: "blue",
+                  },
+                }}
+              >
+                <ListItemText primary="Shop" />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+          <NavLink to="/features" style={navLinkStyles}>
+            <ListItem disablePadding>
+              <ListItemButton
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "white",
+                    borderRadius: "5px",
+                    color: "blue",
+                  },
+                }}
+              >
+                <ListItemText primary="Features" />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+          <NavLink to="/blog" style={navLinkStyles}>
+            <ListItem disablePadding>
+              <ListItemButton
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "white",
+                    borderRadius: "5px",
+                    color: "blue",
+                  },
+                }}
+              >
+                <ListItemText primary="Blog" />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+          <NavLink to="/about" style={navLinkStyles}>
+            <ListItem disablePadding>
+              <ListItemButton
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "white",
+                    borderRadius: "5px",
+                    color: "blue",
+                  },
+                }}
+              >
+                <ListItemText primary="About" />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+          <NavLink to="/contact" style={navLinkStyles}>
+            <ListItem disablePadding>
+              <ListItemButton
+                sx={{
+                  "&:hover": {
+                    backgroundColor: "white",
+                    borderRadius: "5px",
+                    color: "blue",
+                  },
+                }}
+              >
+                <ListItemText primary="Contact" />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+        </Box>
+        <Box sx={{}}>
+            <SearchIcon  sx={{padding:'0px 20px',fontSize:'30px'}}/>
+            <ShoppingCartIcon sx={{padding:'0px 20px',fontSize:'30px'}}/>
+            <FavoriteBorderIcon  sx={{padding:'0px 100px 0px 20px',fontSize:'30px'}} />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Navbar from '../../components/navbar';
 
 import { useRoutes } from 'react-router-dom';
 import { routes } from '../../routes/routes';
@@ -7,6 +8,7 @@ import { routes } from '../../routes/routes';
 export default () => {
   const content = useRoutes(routes);
   return <>
+  <Navbar/>
   {content}
   </>;
 };
